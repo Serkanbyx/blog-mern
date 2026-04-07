18b31b4dba597aa5b731af53c863d277a11ca1de
Serkanbyx <152509582+Serkanbyx@users.noreply.github.com>
2026-04-07 21:49:27 +0300

    Update User.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -87,12 +87,11 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving (only if modified)
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Compare candidate password with stored hash
